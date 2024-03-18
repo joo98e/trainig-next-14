57985d8a3675bfee7a11b30e276412b4276a0189
Joo98e <jtbeok@gmail.com>
2024-03-18 22:26:53 +0900

    refactor : linker type guard

--- src/utils/InternalLinker.ts
+++ src/utils/InternalLinker.ts
@@ -15,9 +15,9 @@ export default class InternalLinker {
   private static readonly EXAMPLE_V1 = new InternalLinker(() => '/example/v1')
   private static readonly EXAMPLE_V2 = new InternalLinker(() => '/example/v2')
   private static readonly MOVIES = new InternalLinker(() => '/example/movies')
-  private static readonly MOVIE = new InternalLinker((movieId: string) => `/example/movies/${movieId}`)
+  private static readonly MOVIE = new InternalLinker((movieInfo: GetPathnameArguments['movie']) => `/example/movies/${movieInfo.id}`)
 
-  static getPathname<K extends InternalLinkType>(type: InternalLinkType, data: GetPathnameArguments[K]) {
+  static getPathname<K extends InternalLinkType>(type: K, data: GetPathnameArguments[K]) {
     switch (type) {
       case 'home':
         return InternalLinker.HOME.buildUrl()
